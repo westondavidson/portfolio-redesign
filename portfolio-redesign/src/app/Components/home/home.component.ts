6ee1ffe6500f0c65bb0243fb217245fdf2085dd1
Weston Davidson <59481331+westondavidson@users.noreply.github.com>
2021-08-10 09:49:23 -0400

    updated notes

--- portfolio-redesign/src/app/Components/home/home.component.ts
+++ portfolio-redesign/src/app/Components/home/home.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  view: [number, number] = [400, 400];
+
+  label: string = "Months Experience";
+
+  experienceData = [
+    {
+      name: "C#", value: 8
+    },
+    {
+      name: "JavaScript", value: 14
+    },
+    {
+      name: "TypeScript", value: 7
+    },
+    {
+      name: "Java", value: 6
+    },
+    {
+      name: "PL/SQL", value: 3
+    }
+  ]
+
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
